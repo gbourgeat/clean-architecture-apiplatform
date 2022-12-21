c717650a775878de84df7e94585535caf566c8f0
gbourgeat <gary.bourgeat@gmail.com>
2022-12-21 16:40:22 +0100

    Chore: Remove old hack added here for issue with using behind Kong Api Gateway

--- public/index.php
+++ public/index.php
@@ -5,8 +5,5 @@ use App\Common\Infrastructure\Symfony\Kernel;
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    $_SERVER['HTTP_X_FORWARDED_PORT'] = '443';
-    $_SERVER['HTTP_X_FORWARDED_PROTO'] = 'https';
-
     return new Kernel((string) $context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
