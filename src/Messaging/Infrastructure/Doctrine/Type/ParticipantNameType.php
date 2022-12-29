c9384a28f040d5351bea173f718009c420efe438
gbourgeat <gary.bourgeat@gmail.com>
2022-12-29 18:13:18 +0100

    Create doctrine type participant_name for ValueObject ParticipantName Add type to doctrine config to be registered Update Mapping definition of Participant Entity to create field in database and remove constraint on "user" field for context isolation, register only "user_id" and add index on it for retrieve with good performance

---
+++ src/Messaging/Infrastructure/Doctrine/Type/ParticipantNameType.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Messaging\Infrastructure\Doctrine\Type;
+
+use App\Common\Infrastructure\Doctrine\Type\StringType;
+use App\Messaging\Domain\ValueObject\ParticipantName;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+
+class ParticipantNameType extends StringType
+{
+    public const TYPE = 'participant_name';
+
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        return ParticipantName::fromString((string) $value);
+    }
+}
