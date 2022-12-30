ec389ec3ca0e8ca8a14e565b4b643b5f10489126
gbourgeat <gary.bourgeat@gmail.com>
2022-12-30 10:48:49 +0100

    Implement "GetMessagesQueryHandler" with previous fix and improve about conversation messages

--- src/Messaging/Application/UseCase/GetMessages/GetMessagesQueryHandler.php
+++ src/Messaging/Application/UseCase/GetMessages/GetMessagesQueryHandler.php
@@ -5,11 +5,43 @@ declare(strict_types=1);
 namespace App\Messaging\Application\UseCase\GetMessages;
 
 use App\Common\Application\Query\QueryHandler;
+use App\Messaging\Application\DTO\MessageDTO;
+use App\Messaging\Domain\Entity\Message;
+use App\Messaging\Domain\Repository\MessageRepository;
+use App\Messaging\Domain\ValueObject\ConversationId;
 
 final class GetMessagesQueryHandler implements QueryHandler
 {
+    public function __construct(
+        private readonly MessageRepository $messageRepository,
+    ) {
+    }
+
+    /**
+     * @return MessageDTO[]
+     */
     public function __invoke(GetMessagesQuery $query): array
     {
-        return [];
+        $conversationId = ConversationId::fromString($query->conversationId);
+
+        $messages = $this->messageRepository
+            ->searchByConversationId(
+                conversationId: $conversationId,
+                page: $query->page,
+                itemsPerPage: $query->itemsPerPage,
+            );
+
+        return $this->mapMessagesToMessagesDTOs($messages);
+    }
+
+    /**
+     * @param Message[] $messages
+     * @return MessageDTO[]
+     */
+    private function mapMessagesToMessagesDTOs(array $messages): array
+    {
+        return array_map(static function (Message $message) {
+            return MessageDTO::fromEntity($message);
+        }, $messages);
     }
 }
